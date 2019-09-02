24058fcd2c0b2b954d0578e4b892fa82f9f9596d
Jack Maldonado <jackmaldonado@Jacks-MBP.attlocal.net>
2019-09-01 23:46:01 -0700

    objects

--- app.ts/app.js
+++ app.ts/app.js
@@ -6,3 +6,24 @@ function returnMyName() {
     return myName;
 }
 console.log(returnMyName());
+function multiply(val1, val2) {
+    return val1 * val2;
+}
+// function type
+// let myMultiply: (val1: number, val2: number) => number;
+// myMultiply = sayHello;
+// myMultiply();
+var myMultiply = multiply;
+console.log(myMultiply(38, 23));
+//Objects
+var userData = {
+    name: "Jack",
+    age: 25
+};
+// complex objects
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
--- app.ts/app.ts
+++ app.ts/app.ts
@@ -11,13 +11,30 @@ function returnMyName(): string {
 console.log(returnMyName());
 
 function multiply(val1: number, val2: number): number {
-  return value1 * value2;
+  return val1 * val2;
 }
 
 // function type
-let myMultiply: (val1: number, val2: number) => ;
+// let myMultiply: (val1: number, val2: number) => number;
 // myMultiply = sayHello;
 // myMultiply();
 
-myMultiply = multiply;
+let myMultiply = multiply;
 console.log(myMultiply(38, 23));
+
+
+//Objects
+let userData: {name: string, age: number} = {
+  name: "Jack",
+  age: 25
+}
+
+// complex objects
+
+let complex: {data: number[], output: (all: boolean) => number[]} = {
+  data: [100, 3.99, 10],
+
+  output: function (all: boolean): number[]{
+    return this.data;
+  }
+};
