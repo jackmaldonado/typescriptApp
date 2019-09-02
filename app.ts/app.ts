8984cb3d1848224dda5bf310da9b1b1e20f65d51
Jack Maldonado <jackmaldonado@Jacks-MBP.attlocal.net>
2019-09-02 00:20:12 -0700

    Using types for a better code

--- app.ts/app.ts
+++ app.ts/app.ts
@@ -38,3 +38,60 @@ let complex: {data: number[], output: (all: boolean) => number[]} = {
     return this.data;
   }
 };
+
+// type alias
+
+type Complex = {data: number[], output: (all: boolean) => number[]};
+
+let complex2: Complex = {
+  data: [100, 3.99, 10],
+
+  output: function (all: boolean): number[]{
+    return this.data;
+  }
+};
+
+// union types
+let myRealRealAge: number | string = 27;
+
+// check types
+
+let finalValue = 30;
+if(typeof finslvalue == "number"){
+  console.log("Final value is a number");
+}
+
+
+// never type
+//
+function neverReturns():never {
+  throw new Error('An error!');
+}
+
+// Nullable types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+
+
+
+// Practice
+
+type Bank = {money: number, deposit: (val: number ) => void};
+
+let bankAccount: Bank = {
+  money: 2000,
+  deposit(value: number): void {
+    this.money += value;
+  }
+};
+
+let myself: {name: string, bankAccount: Bank, hobbies: string[]} = {
+  name: "Max",
+  bankAccount: bankAccount,
+  hobbies: ["Sports", "Cooking"]
+};
+
+myself.bankAccount.deposit(3000);
+console.log(myself);
